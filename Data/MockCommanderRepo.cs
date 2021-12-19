88119f5b1cf83750607754c12423832e36d92734
Usman Yousuf <k180159@nu.edu.pk>
2021-12-19 18:20:44 +0500

    first commit

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>{
+                new Command{Id=0,HowTo="Boil an Egg",Line="Boil Water",Platform="Kettle & Pan"},
+                new Command{Id=1,HowTo="cut Bread",Line="Get a Knife",Platform="Knife and chopping board"},
+                new Command{Id=2,HowTo="make a cup of tea",Line="Place teabag in cup",Platform="Kettle & cup"},
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Kettle & Pan" };
+        }
+    }
+}
